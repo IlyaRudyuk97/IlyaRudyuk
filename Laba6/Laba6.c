b12921f0a6764c76471e2b70afa4c4768ee87000
Илья <rudilya944@gmail.com>
2019-08-16 15:00:06 +0700

    Шестая лаба

--- Laba6/Laba6.c
+++ Laba6/Laba6.c
@@ -18,137 +18,152 @@
 #define ONE_PORTION_OF_HONEY 10
 #define ONE_BEE_BRINGS 2
 #define DEN 20
-
-int count(int n) {
-    int count=0;
-    while (n)
-    {
-        n/=10;
-        count++;
-    }
-    return count;
-}
+#define PATH "amount_of_honey_in_the_den.txt"
 
 int reading_file() {
-    char* file_name = "/home/ilya/Чистяков/Лабы/Laba6/amount_of_honey_in_the_den.txt";
+    struct flock lock;
+    memset(&lock, 0, sizeof(lock));
+
+    char* file_name = PATH;
     int myfile = open (file_name, O_RDONLY);
-    lockf(myfile, F_LOCK, 0L);
 
-    int* buffer = malloc(sizeof(int)*10);
+    int buffer;
 
-    read(myfile, buffer, sizeof(int));
+    lock.l_type = F_RDLCK;
+    fcntl(myfile, F_SETLKW, &lock);
 
-    lockf(myfile, F_ULOCK, 0L);
+    read(myfile, &buffer, sizeof(int));
+    lseek(myfile, 0, 0);
+
+    lock.l_type = F_UNLCK;
+    fcntl(myfile, F_SETLKW, &lock);
     close(myfile);
-    return *buffer;
+
+    return buffer;
 }
 
-void writing_to_file(int amount_of_honey_in_the_den) {
-    char* file_name = "/home/ilya/Чистяков/Лабы/Laba6/amount_of_honey_in_the_den.txt";
-    int myfile = open (file_name, O_WRONLY);
-    lockf(myfile, F_LOCK, 0L);
+void reading_and_writing_to_file(int act) {
+    struct flock lock;
+    memset(&lock, 0, sizeof(lock));
 
-    int length = count(amount_of_honey_in_the_den);
+    char* file_name = PATH;
+    int myfile = open(file_name, O_RDWR);
 
-    write(myfile, (char*)(&amount_of_honey_in_the_den), length);
-    lockf(myfile, F_ULOCK, 0L);
+    int buffer;
+
+    lock.l_type = F_WRLCK;
+    fcntl(myfile, F_SETLKW, &lock);
+
+    read(myfile, &buffer, sizeof(int));
+    lseek(myfile, 0, 0);
+    if (act == 1) {
+        buffer = buffer + ONE_BEE_BRINGS;
+    }
+    else if (act == -1) {
+        buffer = buffer - ONE_PORTION_OF_HONEY;
+    } else if (act == 0) {
+        buffer = DEN;
+    }
+    write(myfile, &buffer, sizeof(int));
+    lseek(myfile, 0, 0);
+
+    lock.l_type = F_UNLCK;
+    fcntl(myfile, F_SETLKW, &lock);
     close(myfile);
 }
 
-void bear() {
-    sleep(100);
+void bear_create() {
+    sleep(5);
     while (1)
     {
         int amount_of_honey_in_the_den = reading_file();
-        printf("Меда в берлоге %d\n", amount_of_honey_in_the_den);
-
         if (amount_of_honey_in_the_den >= ONE_PORTION_OF_HONEY)
         {
-            amount_of_honey_in_the_den = amount_of_honey_in_the_den - ONE_PORTION_OF_HONEY;
-            printf("Меда в берлоге после того как медведь поел %d\n", amount_of_honey_in_the_den);
-            writing_to_file(amount_of_honey_in_the_den);
+            reading_and_writing_to_file(-1);
             printf("Медведь поел и в целом доволен жизнью\n");
-            printf("Меда в берлоге %d\n", amount_of_honey_in_the_den);
+            fflush(stdout);
+            amount_of_honey_in_the_den = reading_file();
+            printf("Меда в берлоге осталось %d\n", amount_of_honey_in_the_den);
+            fflush(stdout);
             sleep(5);
         }
         else
         {
             sleep(5);
-            printf ("Медведь умер\n");
+            reading_and_writing_to_file(0);
             break;
         }
     }
+    exit(0);
 }
 
-int main(int argc, char** argv) {
-
-    int amount_of_honey_in_the_den = 0;
+int main() {
     int amount_of_bees;
-    int stat;
 
-    creat("/home/ilya/Чистяков/Лабы/Laba6/amount_of_honey_in_the_den.txt",__S_IREAD|__S_IWRITE);
-    writing_to_file(amount_of_honey_in_the_den);
+    creat(PATH,__S_IREAD|__S_IWRITE);
+    int amount_of_honey_in_the_den = 0;
+    char* file_name = PATH;
+
+    int myfile = open(file_name, O_WRONLY);
+    write(myfile, &amount_of_honey_in_the_den, sizeof(int));
+    lseek(myfile, 0, 0);
+    close(myfile);
 
     printf("Введите количество пчел ");
     scanf("%d", &amount_of_bees);
 
-
     pid_t bee[amount_of_bees];
 
     for (int i = 0; i < amount_of_bees; i++) {
+
         bee[i] = fork();
         srand(getpid());
 
         if (-1 == bee[i]) {
-            perror("fork");
+            perror("fork bee");
             exit(1);
         }
 
         else if (0 == bee[i]) {
             while (1) {
                 amount_of_honey_in_the_den = reading_file();
-                printf("                                    Меда в берлоге %d\n", amount_of_honey_in_the_den);
-                if (amount_of_honey_in_the_den < (DEN - ONE_BEE_BRINGS)) {
-
-                    printf("ПЧЕЛА №%3.d полетела за медом\n", i+1);
-                    sleep(rand() % 5);
-                    printf("ПЧЕЛА №%3.d вернулась\n", i+1);
-                    sleep(rand() % 1);
-
-                    amount_of_honey_in_the_den = amount_of_honey_in_the_den + ONE_BEE_BRINGS;
-                    writing_to_file(amount_of_honey_in_the_den);
+                printf("ПЧЕЛА №%2.d полетела за медом\n", i+1);
+                sleep(rand() % 5);
+                printf("ПЧЕЛА №%2.d вернулась\n", i+1);
+                sleep(rand() % 1);
+                amount_of_honey_in_the_den = reading_file();
+                if (amount_of_honey_in_the_den < DEN) {
+                    reading_and_writing_to_file(1);
                 }
                 else {
-                                printf("Венрнергрешел\n");
                     break;
                 }
             }
             exit(i+1);
-            printf("Вышел\n");
         }
     }
 
+    pid_t bear;
+    bear = fork();
+    if (-1 == bear) {
+        perror("fork bear");
+        exit(1);
+    }
+    else if (0 == bear) {
+        bear_create();
+    }
 
+    int return_code_bee;
     for (int i = 0; i < amount_of_bees; i++) {
-        int status = waitpid(bee[i], &stat, 1);
-        if (bee[i] == status) {
-            printf("Пчела №%d полетела к другому медведю\n", WEXITSTATUS(stat));
-        }
+        wait(&return_code_bee);
+        printf("Пчела №%d полетела к другому медведю\n", WEXITSTATUS(return_code_bee));
     }
 
-  //  bear();
+    int return_code_bear;
+    waitpid(bear, &return_code_bear, 0);
+    printf("Медведь умер\n");
 
-    //for (int i = 0; i < amount_of_bees; i++) {
-    //int status = waitpid(bee[i], &stat, 0);
-    //if (bee[i] == status) {
-    //printf("\n\nХранилище заполнено медом\n\n");
-    //}
-    //}
-
-//удаление пчел
-
-
-    //remove ("/home/ilya/Чистяков/Лабы/Laba6/amount_of_honey_in_the_den.txt");
-    //printf("Удалено\n");
-    //exit(0);
+    remove (PATH);
+    exit(0);
 }
+
